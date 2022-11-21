6f78a4dabfed414befcfbadd43804435a5d8daf0
Jyunji Kondo <jyukon@gmail.com>
2022-11-21 11:40:35 +0900

    Change specifying parameters in command line

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+"""Usage:
+    mgaonline <token> <lat> <lon> <alt> <pacc>
+
+Options:
+    <token>
+    <lat>   : in degree
+    <lon>   : in degree
+    <alt>   : in meter
+    <pacc>  : position accuracy in meter
+"""
+
 import sys
 import os
 import time
@@ -10,6 +21,7 @@ from serial import Serial
 from mgaonline import MgaOnline
 from ubxmsg import UbxMsg
 from serial.tools import list_ports
+from docopt import docopt
 
 def get_port_name() -> str:
   if sys.platform.startswith('win'):
@@ -51,11 +63,12 @@ def wait_for_mga_ack(ser: Type[Serial]) -> None:
     print(f'The message was not used by the receiver. {MGA_ACK_INFO_STR[mes[6]]}.')
 
 if __name__ == '__main__':
-  token = os.environ['AN_TOKEN']
-  lat = os.environ.get('AN_LAT', 0) # deg
-  lon = os.environ.get('AN_LON', 90) # deg
-  alt = os.environ.get('AN_ALT', 0) # meter unit
-  pacc = os.environ.get('AN_PACC', 100) # meter unit 
+  args = docopt(__doc__)
+  token = args['<token>']
+  lat = args['<lat>']
+  lon = args['<lon>']
+  alt = args['<alt>']
+  pacc = args['<pacc>']
 
   rst_msg = UbxMsg(0x06, 0x04, [0xff, 0xff, 0x02, 0x00]).msg
   ackaiding_msg = UbxMsg(0x06, 0x8a, [0x00, 0x01, 0x00, 0x00, 0x25, 0x00, 0x11, 0x10, 0x01]).msg
